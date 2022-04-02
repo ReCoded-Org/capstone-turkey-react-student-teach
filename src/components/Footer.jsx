4d19cf503b9b219268d8c7e89574435a723a6fdd
Oguz <yilmazoguz@outlook.com>
2022-04-02 23:12:28 +0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <form action="">
+                <label htmlFor="">
+                    Name:
+                    <input type="email" name="name" id="emailfield" />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </>
+    )
+}
