218f47882abd36c9ef1e2e244686bd30a92f6e67
Enes Koç <suateneskoc@gmail.com>
2022-04-05 12:06:07 +0300

    swapped spinner icon with font awesome

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Dialog, Transition } from '@headlessui/react';
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
+import { FaSpinner } from 'react-icons/fa';
 
 import FormField from '../FormField/FormField';
 
@@ -115,34 +116,17 @@ function SignIn({ open, setOpen }) {
                       />
                       <div className="flex justify-between items-center mt-7">
                         {isSubmitting ? (
+                          // Submitting
                           <button
                             type="submit"
                             className="text-lg bg-red-700 text-gray-200 rounded flex items-center px-5 py-2"
                             disabled
                           >
-                            <svg
-                              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                            >
-                              <circle
-                                className="opacity-25"
-                                cx="12"
-                                cy="12"
-                                r="10"
-                                stroke="currentColor"
-                                strokeWidth="4"
-                              />
-                              <path
-                                className="opacity-75"
-                                fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                              />
-                            </svg>
+                            <FaSpinner className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" />
                             Signing in...
                           </button>
                         ) : (
+                          // Idle
                           <button
                             type="submit"
                             className="text-lg text-gray-100 bg-red-600 hover:bg-red-700 transition rounded px-5 py-2"
