7bb1f42146e022e4fb0fd9b5742933c9e7a5107e
Ali <iqalisaleh@gmail.com>
2022-04-05 20:01:36 +0300

    added form structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Contact from './pages/contact-page/Contact';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Contact />
     </div>
   );
 }
---
+++ src/pages/contact-page/Contact.jsx
@@ -0,0 +1,75 @@
+import { Field, useFormik } from 'formik';
+
+function Contact() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      text: '',
+      checked: [],
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  return (
+    <section className="mt-5">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="flex flex-col justify-center items-center bg-red-200"
+      >
+        <label htmlFor="name" className="flex flex-col items-start mt-7">
+          Name
+          <input
+            id="name"
+            type="text"
+            placeholder="Your name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+            className="w-[11.3rem] p-1  mt-2 text-[12px] text-left"
+          />
+        </label>
+        <label htmlFor="email" className="flex flex-col items-start mt-7">
+          Email
+          <input
+            id="email"
+            type="email"
+            placeholder="Your email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+            className="w-[11.3rem] p-1  mt-2 text-[12px] text-left"
+          />
+        </label>
+        <label htmlFor="text" className="flex flex-col items-start mt-7">
+          How can we help?
+          <textarea
+            id="text"
+            type="text"
+            placeholder="Tell us about the project..."
+            onChange={formik.handleChange}
+            value={formik.values.text}
+            className="w-[11.3rem] p-1 pb-[8rem] mt-2 text-[12px] text-left"
+          />
+        </label>
+        <div>
+          <label htmlFor="website-design">
+            Website Design
+            <input
+              type="checkbox"
+              name="checked"
+              value="website-design"
+              id="website-design"
+            />
+          </label>
+        </div>
+
+        <button type="submit" className="px-[4.13rem] py-1 mt-2 bg-cusOrange">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
