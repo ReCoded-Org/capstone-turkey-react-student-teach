db3697bfbd322047a5e8be8555e8d0c5d1b153aa
Ali <iqalisaleh@gmail.com>
2022-04-05 21:41:33 +0300

    removed contact us from navbar branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import Contact from './pages/contact-page/Contact';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Contact />
     </div>
   );
 }
