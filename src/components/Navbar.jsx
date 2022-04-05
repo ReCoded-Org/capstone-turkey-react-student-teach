8a94d0507580c82f2f3a959af511f535a53ea2ba
Ali <iqalisaleh@gmail.com>
2022-04-05 03:33:10 +0300

    changed mobile screen style and added placeholder logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,59 +2,79 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { VscChromeClose } from 'react-icons/vsc';
+import { FcCdLogo } from 'react-icons/fc';
 
 function Navbar() {
   const [dark, setDark] = useState(false);
   const [burger, setBurger] = useState(true);
 
   return (
-    <nav className="flex flex-col justify-center items-center">
-      {burger ? (
-        <GiHamburgerMenu
-          onClick={() => setBurger(!burger)}
-          className="burger-icons"
-        />
-      ) : (
-        <VscChromeClose
-          onClick={() => setBurger(!burger)}
-          className="burger-icons"
-        />
-      )}
+    <nav className="flex flex-col justify-center items-center h-[55px] relative">
+      <div className="flex flex-row-reverse justify-around items-center w-screen mt-3 lg:mt-0">
+        <div>
+          {burger ? (
+            <GiHamburgerMenu
+              onClick={() => setBurger(!burger)}
+              className="burger-icons"
+            />
+          ) : (
+            <VscChromeClose
+              onClick={() => setBurger(!burger)}
+              className="burger-icons"
+            />
+          )}
+        </div>
+        <div className="inline-block lg:hidden lg:mr-[6rem] hover:scale-110 ease-in-out transition-all">
+          <Link
+            to="/question+"
+            className="p-[7px] rounded-md border-[1px] border-cusOrange lg:p-2 "
+          >
+            <span className="text-sm text-cusOrange lg:text-base">
+              Ask Question+
+            </span>
+          </Link>
+        </div>
+        <FcCdLogo className="block text-[2rem] lg:hidden" />
+      </div>
+
       <div
-        className={`lg:flex lg:justify-between lg:items-center w-screen lg:h-[4rem] ${
+        onClick={() => setBurger(true)}
+        aria-hidden="true"
+        className={`ml-3 w-screen top-[4rem] lg:ml-0 lg:flex lg:justify-between lg:items-center lg:h-[4rem] lg:top-[1px] absolute ${
           burger ? 'hidden' : null
-        } lg:opacity-100 transition-all ease-in-out duration-300`}
+        }`}
       >
         <div>
-          <ul className="mt-4 text-2xl lg:mt-0 lg:text-base lg:flex lg:items-center lg:ml-10">
-            <li className="mb-3 lg:pr-3 lg:border-r-[1px] lg:border-cusOrange hover:text-cusOrange  transition-all ease-in-out">
+          <ul className="inline-block mt-4 text-2xl lg:mt-0 lg:text-base lg:flex lg:items-center">
+            <FcCdLogo className="hidden mb-3 lg:mb-0 mx-10 text-[2rem] fill-current text-cusOrange lg:block" />
+            <li className="mb-3 lg:mb-0 lg:pr-3 lg:border-r-[1px] lg:border-cusOrange hover:text-cusOrange transition-all ease-in-out">
               <Link to="/">Home</Link>
             </li>
-            <li className="mb-3 lg:px-3 lg:border-r-[1px] lg:border-cusOrange hover:text-cusOrange transition-all ease-in-out">
+            <li className="mb-3 lg:mb-0 lg:px-3 lg:border-r-[1px] lg:border-cusOrange hover:text-cusOrange transition-all ease-in-out">
               <Link to="/questions">Questions</Link>
             </li>
-            <li className="mb-3 lg:px-3 lg:border-r-[1px] lg:border-cusOrange hover:text-cusOrange transition-all ease-in-out">
+            <li className="mb-3 lg:mb-0 lg:px-3 lg:border-r-[1px] lg:border-cusOrange hover:text-cusOrange transition-all ease-in-out">
               <Link to="/contact">Contact</Link>
             </li>
-            <li className="mb-3 lg:px-3 hover:text-cusOrange transition-all ease-in-out">
+            <li className="mb-3 lg:mb-0 lg:px-3 hover:text-cusOrange transition-all ease-in-out">
               <Link to="/about">About</Link>
             </li>
           </ul>
         </div>
 
-        <div className="mt-8 lg:mt-0 hover:scale-110 ease-in-out transition-all">
+        <div className="hidden mt-5 lg:mt-0 lg:mr-[6rem] lg:inline-block hover:scale-110 ease-in-out transition-all">
           <Link
             to="/question+"
-            className="cursor-pointer max-w-[10rem] p-3 lg:p-2 rounded-md  bg-cusOrange "
+            className="max-w-[10rem] p-3 rounded-md bg-cusOrange lg:p-2"
           >
-            <span className="text-2xl lg:text-base text-white">
+            <span className="text-2xl text-white lg:text-base">
               Ask Question+
             </span>
           </Link>
         </div>
 
-        <div className=" flex flex-col-reverse lg:flex lg:flex-row lg:item-center">
-          <div className="flex justify-center mt-5 ml-[3.7rem] lg:mt-0 item-center mr-10">
+        <div className="flex flex-col-reverse lg:flex lg:flex-row lg:item-center">
+          <div className="flex justify-center item-center mt-5 ml-[3.7rem] mr-10 lg:mt-0">
             <label
               htmlFor="toggleB"
               className="flex item-center cursor-pointer"
@@ -67,20 +87,18 @@ function Navbar() {
                   className="sr-only"
                 />
 
-                <div className="bgColor block border-[2px] lg:border-[1px] border-cusOrange border-black-600 w-[4rem] lg:w-[3rem] h-6 rounded-md" />
+                <div className="bgColor block border-[2px] rounded-md border-cusOrange w-[4rem] h-6 lg:border-[1px] lg:w-[3rem]" />
 
-                <div className="dot absolute left-1 top-1 bg-no-repeat w-9 lg:w-6 h-4 rounded-3xl transition" />
+                <div className="dot left-1 top-1 bg-no-repeat w-9 h-4 rounded-3xl lg:w-6 transition absolute" />
               </div>
-
-              <div className="ml-3 text-red-700 font-medium" />
             </label>
           </div>
-          <div>
-            <ul className="mt-8 text-2xl  lg:text-base lg:mt-0 flex items-center justify-center ml-10">
+          <div className="inline-block">
+            <ul className="flex justify-center items-center mt-2 ml-10 text-2xl lg:text-base lg:mt-0">
               <li>
                 <Link
                   to="/signup"
-                  className="pr-3 border-r-[2px] lg:border-r-[1px] border-cusOrange hover:text-cusOrange transition-all ease-in-out"
+                  className="pr-3 border-r-[2px] border-cusOrange lg:border-r-[1px] hover:text-cusOrange transition-all ease-in-out inline-block"
                 >
                   Sign Up
                 </Link>
@@ -88,7 +106,7 @@ function Navbar() {
               <li>
                 <Link
                   to="/signin"
-                  className="cursor-pointer pl-3 mr-10 hover:text-cusOrange transition-all ease-in-out"
+                  className="pl-3 mr-10 hover:text-cusOrange transition-all ease-in-out inline-block"
                 >
                   Sign In
                 </Link>
