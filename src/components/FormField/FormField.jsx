b8c46d8e11ee6e22921ca13e93190680209f7bb6
Enes Koç <suateneskoc@gmail.com>
2022-04-07 11:50:23 +0300

    added support for textarea type in FormField

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -3,19 +3,38 @@
 import PropTypes from 'prop-types';
 import { ErrorMessage, Field } from 'formik';
 
-function FormField({ autoComplete = '', className = '', label, name, type }) {
+function FormField({
+  autoComplete = '',
+  className = '',
+  label,
+  name,
+  rows,
+  type,
+}) {
   return (
     <div className={className}>
       <label htmlFor={name} className="block text-sm font-medium text-gray-700">
         {label}
       </label>
-      <Field
-        type={type}
-        id={name}
-        name={name}
-        autoComplete={autoComplete}
-        className="my-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-      />
+      {type === 'textarea' ? (
+        <Field
+          as={type}
+          autoComplete={autoComplete}
+          className="my-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+          id={name}
+          name={name}
+          rows={rows}
+        />
+      ) : (
+        <Field
+          autoComplete={autoComplete}
+          className="my-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+          id={name}
+          name={name}
+          type={type}
+        />
+      )}
+
       <ErrorMessage
         className="text-sm text-red-600"
         component="div"
@@ -29,11 +48,13 @@ FormField.propTypes = {
   className: PropTypes.string,
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  rows: PropTypes.string,
   type: PropTypes.string.isRequired,
 };
 FormField.defaultProps = {
   autoComplete: '',
   className: '',
+  rows: '5',
 };
 
 export default FormField;
