22ee4a172a162c1afe6cc91430f9e642057c0864
Ali <iqalisaleh@gmail.com>
2022-04-07 15:34:53 +0300

    added contact info responsive

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -23,8 +23,8 @@ function Contact() {
   });
 
   return (
-    <div>
-      <section className="mt-5">
+    <div className="lg:flex lg:flex-row-reverse lg:justify-end lg:h-screen">
+      <section className="pt-5">
         <form
           onSubmit={formik.handleSubmit}
           className="flex flex-col justify-center items-center bg-red-200"
@@ -80,44 +80,53 @@ function Contact() {
         </form>
       </section>
 
-      <section className="mt-5 bg-cusOrange">
-        <div className="p-4">
+      <section className="mt-5 bg-cusOrange lg:flex lg:flex-col lg:items-start lg:pr-[6rem] ">
+        <div className="p-4 lg:mt-[5rem] lg:text-left lg:ml-10">
           <h1 className="font-bold">Get in touch</h1>
-          <h4 className="mt-2 ">
-            We love to hear from you, Our team is always here to chat.
+          <h4 className="mt-2">
+            We love to hear from you,
+            <br /> Our team is always here to chat.
           </h4>
         </div>
-        <div className="flex flex-col bg-cusOrange p-4 pt-3">
+        <div className="flex flex-col bg-cusOrange p-4 pt-3 lg:flex lg:flex-row lg:items-start lg:text-left lg:ml-10">
           <div className="flex justify-center items-center mr-3">
-            <FaMediumM className="" />
-            <h2 className="font-bold ml-2">Email</h2>
+            <FaMediumM className="lg:mt-1" />
+            <h2 className="font-bold ml-2 block lg:hidden">Email</h2>
           </div>
-          <h3>Contact us via email</h3>
-          <h3>hi@untitiledui.com</h3>
+          <div className="lg:flex lg:flex-col lg:items-start">
+            <h2 className="font-bold ml-2 lg:m-0 lg:block hidden">Email</h2>
+            <h3>Contact us via email</h3>
+            <h3>hi@untitiledui.com</h3>
+          </div>
         </div>
 
-        <div className="flex flex-col bg-cusOrange p-4">
+        <div className="flex flex-col bg-cusOrange p-4 lg:flex lg:flex-row lg:items-start lg:text-left lg:ml-10">
           <div className="flex justify-center items-center mr-3">
-            <FaPhoneAlt className=" " />
-            <h2 className="font-bold ml-2">Phone</h2>
+            <FaPhoneAlt className="lg:mt-1" />
+            <h2 className="font-bold ml-2 block lg:hidden">Phone</h2>
           </div>
-          <h3>Contact us via phone</h3>
-          <h3>552-555-5555</h3>
-        </div>
-        <div className="flex flex-col bg-cusOrange p-4">
-          <div className="flex justify-center items-cente ">
-            <FaMapMarker className="" />
-            <h2 className="font-bold ml-2">Location</h2>
+          <div className="lg:flex lg:flex-col lg:items-start">
+            <h2 className="font-bold ml-2 lg:m-0 lg:block hidden">Phone</h2>
+            <h3>Contact us via phone</h3>
+            <h3>552-555-5555</h3>
           </div>
-          <h3>Turkey, Ankara</h3>
-          <h3>36 Street, Bahcelievler</h3>
         </div>
-
-        <div className="flex justify-center items-center p-3">
-          <FaInstagram className="mx-3 text-xl" />
-          <FaGithub className="mx-3 text-xl" />
-          <FaLinkedinIn className="mx-3 text-xl" />
-          <FaYoutube className="mx-3 text-xl" />
+        <div className="flex flex-col bg-cusOrange p-4 lg:flex lg:flex-row lg:items-start lg:text-left lg:ml-10">
+          <div className="flex justify-center items-center mr-3">
+            <FaMapMarker className="lg:mt-1" />
+            <h2 className="font-bold ml-2 lg:m-0 block lg:hidden">Location</h2>
+          </div>
+          <div className="lg:flex lg:flex-col lg:items-start">
+            <h2 className="font-bold ml-2 lg:m-0 lg:block hidden">Location</h2>
+            <h3>Turkey, Ankara</h3>
+            <h3>36 Street, Bahcelievler</h3>
+          </div>
+        </div>
+        <div className="flex justify-center items-center p-3 lg:mt-[24rem] lg:ml-[5rem]">
+          <FaInstagram className="cursor-pointer mx-3 text-xl lg:text-3xl lg:mx-5" />
+          <FaGithub className="cursor-pointer mx-3 text-xl lg:text-3xl lg:mx-5" />
+          <FaLinkedinIn className="cursor-pointer mx-3 text-xl lg:text-3xl lg:mx-5" />
+          <FaYoutube className="cursor-pointer mx-3 text-xl lg:text-3xl lg:mx-5" />
         </div>
       </section>
     </div>
