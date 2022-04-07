b5925878b336c5d4c99042e66299feb0a1b941dd
hazar-nas <73062961+hazar-nas@users.noreply.github.com>
2022-04-07 21:05:27 +0300

    6 add bookmark page (#13)
    * bookmarkpage-remove-and-load-items
    
    * fixed-issues
    
    * eslint-issue
    
    * -eslint-issue-2
    
    * remove-and-load-items
    
    * revised-errors
    
    * test-passed
    
    * use-effect-issue
    
    * revisions-to-branch
    
    * dependency-rearranged
    
    * no-test-implemented
    
    * test-implemented
    
    * redux-toolkit-moved-to-dependency
    
    * react-renderer-up-top
    
    * moved-to-up
    
    * last-changes
    
    * chore: cleanup tailwind plugins
    
    * Chore: ignore tailwind config
    
    Co-authored-by: Muhanned Noman <Muhanned.Noman@gmail.com>

---
+++ src/pages/mockData.js
@@ -0,0 +1,118 @@
+import Photo from '../assets/images/avatar.jpg';
+
+const data = [
+  {
+    id: 1,
+    avatar: Photo,
+    name: 'Maria1223',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 2,
+    avatar: Photo,
+    name: 'Name',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 3,
+    avatar: Photo,
+    name: 'Merlen',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 4,
+    avatar: Photo,
+    name: 'Asedssa',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s',
+  },
+  {
+    id: 5,
+    avatar: Photo,
+    name: 'Hazar',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 6,
+    avatar: Photo,
+    name: 'Mustafa',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 7,
+    avatar: Photo,
+    name: 'Yusuf',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 8,
+    avatar: Photo,
+    name: 'Erkay',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s',
+  },
+  {
+    id: 9,
+    avatar: Photo,
+    name: 'Maria2',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 10,
+    avatar: Photo,
+    name: 'Maria3',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 11,
+    avatar: Photo,
+    name: 'Maria4',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 12,
+    avatar: Photo,
+    name: 'Maria5',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s',
+  },
+  {
+    id: 13,
+    avatar: Photo,
+    name: 'Maria6',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 14,
+    avatar: Photo,
+    name: 'Maria7',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 15,
+    avatar: Photo,
+    name: 'Maria8',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s test clamp data classname',
+  },
+  {
+    id: 16,
+    avatar: Photo,
+    name: 'Maria9',
+    questionTitle:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys 1500s',
+  },
+];
+
+export default data;
