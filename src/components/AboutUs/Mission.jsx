a843c21a7d1920d60fe5c11871107004cbf4b3a6
Oguz <yilmazoguz@outlook.com>
2022-04-08 16:43:35 +0300

    Add Mission.jsx

---
+++ src/components/AboutUs/Mission.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Mission() {
+    return (
+        <>
+            <div className="container mx-autoo">
+                <h1>Lorem ipsum dolor sit amet.</h1>
+                <div className="columns-4 ">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod aperiam et, neque voluptates iure sed ratione voluptatum odio ab culpa.
+                </div>
+                <img src="" alt="hands-img" className="columns-6" />
+            </div>
+
+        </>
+    )
+}
+
+export default Mission;
---
+++ src/containers/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from './Form';
+
+export const Footer = () => {
+    return (
+        <>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+            <Form/>
+        </>
+    )
+}
