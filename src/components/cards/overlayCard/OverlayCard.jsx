46ad6e5ceb0e2efd723f1a4d547ebd64f0b9c7ca
Ali <iqalisaleh@gmail.com>
2022-04-10 01:18:57 +0300

    fixed overlay card opacity bug

--- src/components/cards/overlayCard/OverlayCard.jsx
+++ src/components/cards/overlayCard/OverlayCard.jsx
@@ -1,6 +1,6 @@
 function OverlayCard() {
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 h-screen w-screen bg-white opacity-[85%gi]" />
+    <div className="fixed top-0 bottom-0 left-0 right-0 h-screen w-screen bg-white opacity-[85%]" />
   );
 }
 
