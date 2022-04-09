e7c3e3a0f4316e4d91740c82e90a12e122036008
Ali <iqalisaleh@gmail.com>
2022-04-09 03:15:13 +0300

    finished creating respnsive contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -13,21 +13,30 @@ import * as Yup from 'yup';
 import FormField from '../../components/FormField/FormField';
 
 const ContactSchema = Yup.object().shape({
-  username: Yup.string().max(24).required(),
+  name: Yup.string().max(24).required(),
+  email: Yup.string().max(24).required(),
+  message: Yup.string(),
 });
 function Contact() {
   return (
-    <div className="lg:flex lg:flex-row-reverse lg:justify-end lg:h-screen">
-      <section>
+    <div className="lg:flex lg:flex-row-reverse lg:justify-end lg:h-screen lg:text-base md:text-base text-sm">
+      <section className="w-screen h-3/4 mt-10 md:h-[60vh] flex flex-col justify-center items-center lg:mt-[10rem]">
+        <h1 className="mb-10 font-semibold text-2xl md:text-3xl lg:text-[2.3rem] lg:mb-[3.5]">
+          Level up Your brand
+        </h1>
         <Formik
           initialValues={{
             name: '',
+            email: '',
+            textarea: '',
+            service: [],
           }}
           validationSchema={ContactSchema}
-          onSubmit={(values, { setSubmitting }) => {
+          onSubmit={(values, { setSubmitting, resetForm }) => {
             setTimeout(() => {
-              console.log(values, 'aa');
+              console.log(values);
               setSubmitting(false);
+              resetForm();
             }, 1000);
           }}
         >
@@ -37,30 +46,91 @@ function Contact() {
             errors,
             isSubmitting,
           }) => (
-            <Form>
+            <Form className="w-[15rem] lg:w-[20rem]">
               <FormField
                 autoComplete="name"
-                className="mb-3 "
+                className="mb-10 lg:text-left"
                 errors={errors}
                 label="Name"
                 name="name"
                 touched={touched}
                 type="text"
+                placeholder="Your Name"
               />
+              <FormField
+                autoComplete="email"
+                className="mb-10 lg:text-left"
+                errors={errors}
+                label="Email"
+                name="email"
+                touched={touched}
+                type="email"
+                placeholder="You@company.com"
+              />
+              <FormField
+                autoComplete="name"
+                className="mb-3 lg:text-left"
+                textfieldClass="lg:pb-[7rem]"
+                errors={errors}
+                label="How can we help?"
+                name="message"
+                touched={touched}
+                type="textarea"
+                placeholder="Tell us about your project"
+              />
+              <h1 className="text-left font-medium">Services</h1>
+              <div className="flex my-5 w-max lg:ml-5">
+                <div className="flex flex-col items-start mr-5 w-max">
+                  <FormField
+                    className="contact-checkbox-field"
+                    errors={errors}
+                    name="service"
+                    label="Web Design"
+                    value="WebDesign"
+                    type="checkbox"
+                  />
+                  <FormField
+                    className="contact-checkbox-field"
+                    errors={errors}
+                    name="service"
+                    label="Content Creation"
+                    value="ContentCreation"
+                    type="checkbox"
+                  />
+                </div>
+                <div className="flex flex-col items-start w-max">
+                  <FormField
+                    className="contact-checkbox-field"
+                    errors={errors}
+                    name="service"
+                    label="UX Desgin"
+                    value="UxDesgin"
+                    type="checkbox"
+                  />
+                  <FormField
+                    className="contact-checkbox-field"
+                    errors={errors}
+                    name="service"
+                    label="Other"
+                    value="Other"
+                    type="checkbox"
+                  />
+                </div>
+              </div>
 
               {isSubmitting ? (
                 <button
                   type="submit"
-                  className="text-lg bg-red-700 text-gray-200 rounded flex items-center px-5 py-2"
+                  className="text-lg bg-cusOrange text-white rounded pl-10 pr-5 py-2 mb-10 lg:mb-0 relative"
                   disabled
                 >
-                  <FaSpinner className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" />
+                  <FaSpinner className="animate-spin h-5 text-white mr-3 absolute right-[7rem] top-[0.8rem]" />
                   Submiting...
                 </button>
               ) : (
                 <button
                   type="submit"
-                  className="text-lg text-gray-100 bg-red-600 hover:bg-red-700 transition rounded px-5 py-2"
+                  className="text-lg text-gray-100 bg-cusOrange transition rounded px-8 py-2 mb-10 lg:mb-0 hover:scale-110 ease-in-out"
                 >
                   Submit
                 </button>
@@ -70,7 +140,7 @@ function Contact() {
         </Formik>
       </section>
 
-      <section className="bg-cusOrange lg:flex lg:flex-col lg:items-start lg:pr-[2rem] ">
+      <section className="bg-cusOrange mt-10 md:h-3/4 lg:h-[100vh] lg:mt-0 lg:flex lg:flex-col lg:items-start lg:pr-[2rem]">
         <div className="p-4 lg:mt-[5rem] lg:text-left lg:ml-10">
           <h1 className="font-bold lg:text-xl">Get in touch</h1>
           <h4 className="mt-2 lg:mt-1">
@@ -78,8 +148,8 @@ function Contact() {
             <br /> Our team is always here to chat.
           </h4>
         </div>
-        <div className="flex flex-col bg-cusOrange p-4 lg:flex lg:flex-row lg:items-start lg:text-left lg:ml-10 lg:mt-5">
-          <div className="flex justify-center items-center mr-3">
+        <div className="contact-info-icon-container">
+          <div className="contact-info-icon">
             <FaMediumM className="lg:mt-1" />
             <h2 className="font-bold ml-2 block lg:hidden">Email</h2>
           </div>
@@ -90,8 +160,8 @@ function Contact() {
           </div>
         </div>
 
-        <div className="flex flex-col bg-cusOrange p-4 lg:flex lg:flex-row lg:items-start lg:text-left lg:ml-10 lg:mt-5">
-          <div className="flex justify-center items-center mr-3">
+        <div className="contact-info-icon-container">
+          <div className="contact-info-icon">
             <FaPhoneAlt className="lg:mt-1" />
             <h2 className="font-bold ml-2 block lg:hidden">Phone</h2>
           </div>
@@ -101,8 +171,8 @@ function Contact() {
             <h3>552-555-5555</h3>
           </div>
         </div>
-        <div className="flex flex-col bg-cusOrange p-4 lg:flex lg:flex-row lg:items-start lg:text-left lg:ml-10 lg:mt-5">
-          <div className="flex justify-center items-center mr-3">
+        <div className="contact-info-icon-container">
+          <div className="contact-info-icon">
             <FaMapMarker className="lg:mt-1" />
             <h2 className="font-bold ml-2 lg:m-0 block lg:hidden">Location</h2>
           </div>
@@ -113,10 +183,10 @@ function Contact() {
           </div>
         </div>
         <div className="flex justify-center items-center p-3 lg:mt-[19rem] lg:ml-[1rem]">
-          <FaInstagram className="social-icons" />
-          <FaGithub className="social-icons" />
-          <FaLinkedinIn className="social-icons" />
-          <FaYoutube className="social-icons" />
+          <FaInstagram className="contact-social-icons" />
+          <FaGithub className="contact-social-icons" />
+          <FaLinkedinIn className="contact-social-icons" />
+          <FaYoutube className="contact-social-icons" />
         </div>
       </section>
     </div>
