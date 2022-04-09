e85c018aba01f963310b66b57320242c2c36a751
Ali Saleh <97849626+aliSalehAbdulhadi@users.noreply.github.com>
2022-04-09 21:32:16 +0300

    5 add contact page (#18)
    * added form structure
    
    * added contact info section
    
    * added contact info mobile screen only
    
    * added contact info responsive
    
    * finished contact info section
    
    * removed changes from dev branch
    
    * fixed import bug
    
    * updated files
    
    * finished creating respnsive contact page
    
    * added contact snapshot test
    
    * removed forgotten console
    
    Co-authored-by: Muhanned Noman <Muhanned.Noman@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import BookmarkPage from './pages/BookMarkPage/BookmarkPage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <BookmarkPage />
     </div>
   );
 }
