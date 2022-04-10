b3c1856d04ff12d21ebe7cb455a0ba0153a77352
hazar-nas <hazarcan.nas@gmail.com>
2022-04-11 00:46:28 +0300

    input-overflow-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Question from './components/Question/Question';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Question />
     </div>
   );
 }
