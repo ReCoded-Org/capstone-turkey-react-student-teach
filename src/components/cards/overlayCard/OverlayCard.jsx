34566426bce06f5d05b3c89182f14696051c2338
Ali <iqalisaleh@gmail.com>
2022-04-11 23:08:03 +0300

    added blur to overlay card

--- src/components/cards/overlayCard/OverlayCard.jsx
+++ src/components/cards/overlayCard/OverlayCard.jsx
@@ -1,6 +1,6 @@
 function OverlayCard() {
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 h-screen w-screen bg-white opacity-[85%]" />
+    <div className="fixed inset-0 bg-white bg-opacity-60 transition-opacity backdrop-blur" />
   );
 }
 
