3560fd9695c435f483a314182dba30c921c23b39
Muhanned Noman <Muhanned.Noman@gmail.com>
2022-04-12 21:08:06 +0300

    Chore: update home route

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import Home from './pages/Home/Home';
 import Questions from './pages/questions/Questions';
 import Contact from './pages/contact/Contact';
 import About from './pages/about/About';
-import { ABOUT_ROUTE, CONTACT_ROUTE, QUESTIONS_ROUTE } from './routes';
+import {
+  ABOUT_ROUTE,
+  CONTACT_ROUTE,
+  QUESTIONS_ROUTE,
+  HOME_ROUTE,
+} from './routes';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={HOME_ROUTE} element={<Home />} />
         <Route path={QUESTIONS_ROUTE} element={<Questions />} />
         <Route path={CONTACT_ROUTE} element={<Contact />} />
         <Route path={ABOUT_ROUTE} element={<About />} />
