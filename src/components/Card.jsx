dc713df476f8852f4343af9741870b2c6221990c
Oguz <yilmazoguz@outlook.com>
2022-04-13 15:26:41 +0300

    Add grid for layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,35 @@
-import PropTypes from 'prop-types';
 
 import githubLogo from '../assets/images/github-logo.png';
 import linkedinLogo from '../assets/images/linkedin-logo.png';
 
-function Card( { name, title, email, photo } ) {
+import PropTypes from 'prop-types';
+
+function Card({ name, title, email, photo }) {
 
     return (
-            <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-4">
-                <img
-                    src={photo} 
-                    alt="profilePicture"
-                    className='w-10 h-10'
-                />
-                <div className="p-5 bg-card-background">
-                    <p className="mb-3 font-normal text-[#424242] dark:text-gray-400">Name: 
-                        {name}
-                    </p>
-                    <p className="mb-3 font-normal text-[#424242] dark:text-gray-400">Title: {title}</p>
-                    <p className="mb-3 font-normal text-[#424242] dark:text-gray-400">Email Address: {email}</p>
-                    <div className="flex flex-row justify-start">
-                        <a href="contact.github" className='mr-4'>
-                            <img src={githubLogo} alt="logo of github" height={24} width={24}/>
-                        </a> 
-                        <a href="contact.linkedin" className='ml-4'>
-                            <img src={linkedinLogo} alt="logo of linkedin" height={24} width={24}/>
-                        </a>
+
+                <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-4">
+                    <img
+                        src={photo} 
+                        alt="profilePicture"
+                        className='w-10 h-10'
+                    />
+                    <div className="p-5 bg-card-background">
+                        <p className="mb-3 font-normal text-[#424242] dark:text-gray-400">Name: 
+                            {name}
+                        </p>
+                        <p className="mb-3 font-normal text-[#424242] dark:text-gray-400">Title: {title}</p>
+                        <p className="mb-3 font-normal text-[#424242] dark:text-gray-400">Email Address: {email}</p>
+                        <div className="flex flex-row justify-start">
+                            <a href="contact.github" className='mr-4'>
+                                <img src={githubLogo} alt="logo of github" height={24} width={24}/>
+                            </a> 
+                            <a href="contact.linkedin" className='ml-4'>
+                                <img src={linkedinLogo} alt="logo of linkedin" height={24} width={24}/>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
     )
 }
 
--- src/pages/AboutUs/About.jsx
+++ src/pages/AboutUs/About.jsx
@@ -5,13 +5,17 @@ import creators from '../../components/creators'
 function About() {
  
   return (
-    <div>
-         <h1>About</h1>
-        {
-            creators.map((creator)=>(
-                <Card key={creator.id} name={creator.name} title={creator.title} email={creator.email} photo={creator.photo}/>
-            ))
-        }
+    <div className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12'>
+         <h1 class="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900 text-center">
+            About
+          </h1>
+         <div className='grid grid-cols-1 lg:grid-cols-3 gap-3'>
+          {
+              creators.map((creator)=>(
+                  <Card key={creator.id} name={creator.name} title={creator.title} email={creator.email} photo={creator.photo}/>
+              ))
+          }
+         </div>
     </div>
   )
 }
