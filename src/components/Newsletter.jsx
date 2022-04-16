b2cba3956a1ed0706c230e3ccfce906848c9fbf5
Oguz <yilmazoguz@outlook.com>
2022-04-16 21:34:59 +0300

    Change branch

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,43 +1,25 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, Field, Form } from 'formik';
 
-const validate = values => {
-  const errors = {};
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
-  return errors;
-};
-
-const Newsletter = () => {
-
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-    },
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
+export default function Newsletter() {
   return (
-    <>
-      <form 
-        className='block flex-col justify-items-center align-middle content-center md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1'
-        onSubmit={formik.handleSubmit}
-      >
-        <input
+    <Formik
+      initialValues={{
+        email: ''
+      }}
+      onSubmit={async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 0))
+      }}
+    >
+      <Form className="block flex-col justify-items-center align-middle content-center md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1">
+        <Field
           id="email"
           name="email"
           type="email"
-          value={formik.values.value}
-          onChange={formik.handleChange}
           placeholder="Your Email Address"
           className="block w-[312px] h-auto text-base font-semibold transition ease-in-out outline outline-1 outline-secondary-color rounded-md text-font-color px-2 py-2 mb-3 text-center justify-center border-secondary-color"
         />
-
         <a href="#" className="inline-flex items-center justify-center w-[468px]">
           <button
             type="submit"
@@ -45,10 +27,7 @@ const Newsletter = () => {
               Submit
           </button>
         </a>
-
-      </form>
-    </>
+      </Form>
+    </Formik>
   )
 }
-
-export default Newsletter
