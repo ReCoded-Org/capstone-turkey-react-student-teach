801793353de088bf48e838cda614e47e39c83897
Oguz <yilmazoguz@outlook.com>
2022-04-16 19:23:32 +0300

    Utilize Formik for the newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import { useState } from 'react';
+import { useFormik } from 'formik';
 
+const validate = values => {
+  const errors = {};
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  return errors;
+};
+s
 const Newsletter = () => {
 
-  const [inputValue, setInputValue] =  useState("");
-
-  const handleChange = (e) => {
-    setInputValue(e.currentTarget.value)
-    e.preventDefault();
-  }
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   return (
     <>
-      <form className='block flex-col justify-items-center align-middle content-center md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1'>
-
-        <input 
-          type="text"
-          value={inputValue}
-          onChange={handleChange}
+      <form 
+        className='block flex-col justify-items-center align-middle content-center md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1'
+        onSubmit={formik.handleSubmit}
+      >
+        <input
+          id="email"
+          name="email"
+          type="email"
+          value={formik.values.value}
+          onChange={formik.handleChange}
           placeholder="Your Email Address"
           className="block w-[312px] h-auto text-base font-semibold transition ease-in-out outline outline-1 outline-secondary-color rounded-md text-font-color px-2 py-2 mb-3 text-center justify-center border-secondary-color"
         />
