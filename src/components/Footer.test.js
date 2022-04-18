e44e38c33998fd6fd93fb188138e557a44a7a17d
Oguz <yilmazoguz@outlook.com>
2022-04-18 21:08:54 +0300

    Conduct snapshot testing for both Newsletter.jsx and Footer.jsx

---
+++ src/components/Footer.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { Footer } from "./Footer";
+
+test("Initial test for Footer.jsx", () => {
+    const tree = renderer. create(<Footer />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
---
+++ src/components/Newsletter.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Newsletter from "./Newsletter";
+
+test("Initial test", () => {
+    const tree = renderer. create(<Newsletter />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
