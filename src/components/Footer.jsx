4bb4792f3696d90af958d19d2d5c8faa97e87551
Oguz <yilmazoguz@outlook.com>
2022-04-22 16:02:49 +0300

    Enable a config in eslintrc.json

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,74 +8,103 @@ export const Footer = () => {
         Subscribe to Our Newsletter
       </h2>
       <Newsletter />
-      <div className="grid lg:grid-cols-4 gap-4 md:grid-cols-1 md:grid-rows-4 xs:grid-rows-4 sm:grid-rows-4 sm:grid-cols-1 mt-16 sm:justify-center xs:justify-center xs:align-middle xs:grid-cols-1
-      xs:pb-8">
-          <div className="flex flex-col xs:justify-items-center xs:align-middle sm:justify-items-center md:justify-items-center lg:justify-center">
-            <img src="recoded-logo.png" className="w-[72px] h-[72px] rounded-full mr-96 ml-[10px] xs:ml-28" alt="logo of recoded" />
-            <div className="p-[10px] text-left text-base w-[screen] max-h-max xs:w-[260px] xs:h-[300px] mx-auto text-black xs:mb-24">
-              This project has been completed by two groups selected for the bootcamps of React and back end conducted by Re:coded respectively. Attendees of the bootcamps have completed their individual tasks by gathering together occasionally 3 times a week. All attendees have been given chances to develop their skills in every possible realm they could possibly face in real world.
-            </div>
+      <div
+        className="grid lg:grid-cols-4 gap-4 md:grid-cols-1 md:grid-rows-4 xs:grid-rows-4 sm:grid-rows-4 sm:grid-cols-1 mt-16 sm:justify-center xs:justify-center xs:align-middle xs:grid-cols-1
+      xs:pb-8"
+      >
+        <div className="flex flex-col xs:justify-items-center xs:align-middle sm:justify-items-center md:justify-items-center lg:justify-center">
+          <img
+            src="recoded-logo.png"
+            className="w-[72px] h-[72px] rounded-full mr-96 ml-[10px] xs:ml-28"
+            alt="logo of recoded"
+          />
+          <div className="p-[10px] text-left text-base w-[screen] max-h-max xs:w-[260px] xs:h-[300px] mx-auto text-black xs:mb-24">
+            This project has been completed by two groups selected for the
+            bootcamps of React and back end conducted by Re:coded respectively.
+            Attendees of the bootcamps have completed their individual tasks by
+            gathering together occasionally 3 times a week. All attendees have
+            been given chances to develop their skills in every possible realm
+            they could possibly face in real world.
           </div>
+        </div>
         <ul className="flex flex-col align-middle m-6 xs:m-1 sm:mt-1">
-          <span className="p-1 font-semibold text-2xl xs:mt-16">Useful Links</span>
+          <span className="p-1 font-semibold text-2xl xs:mt-16">
+            Useful Links
+          </span>
           <a
             href="https://www.re-coded.com/"
             className="mt-9 mb-3 xs:mt-3 inline-block"
           >
-            <li className="text-secondary-color text-base hover:text-font-color">Re:coded</li>
+            <li className="text-secondary-color text-base hover:text-font-color">
+              Re:coded
+            </li>
           </a>
           <a
             href="https://flatironschool.com/"
             className="mt-9 mb-3 xs:mt-3 inline-block"
           >
-            <li className="text-secondary-color text-base hover:text-font-color">Flatiron School</li>
+            <li className="text-secondary-color text-base hover:text-font-color">
+              Flatiron School
+            </li>
           </a>
           <a
-            href="https://github.com/ReCoded-Org/capstone-turkey-react-student-teach" className="mt-9 mb-3 xs:mt-3 inline-block"
+            href="https://github.com/ReCoded-Org/capstone-turkey-react-student-teach"
+            className="mt-9 mb-3 xs:mt-3 inline-block"
           >
-            <li className="text-secondary-color text-base hover:text-font-color">Project Repo</li>
+            <li className="text-secondary-color text-base hover:text-font-color">
+              Project Repo
+            </li>
           </a>
         </ul>
         <ul className="flex flex-col align-middle m-6 xs:m-1 sm:m-1">
           <span className="p-1 font-semibold text-2xl">Useful Links</span>
           <a
-            href='https://reactjs.org/'
+            href="https://reactjs.org/"
             className="mt-9 mb-3 xs:mt-3 inline-block"
           >
-            <li className="text-secondary-color text-base hover:text-font-color">React</li>
+            <li className="text-secondary-color text-base hover:text-font-color">
+              React
+            </li>
           </a>
           <a
             href="https://overreacted.io/"
             className="mt-9 mb-3 xs:mt-3 inline-block"
           >
-            <li className="text-secondary-color text-base hover:text-font-color">Overreacted.io</li>
+            <li className="text-secondary-color text-base hover:text-font-color">
+              Overreacted.io
+            </li>
           </a>
           <a
-            href='https://developer.mozilla.org/en-US/'
+            href="https://developer.mozilla.org/en-US/"
             className="mt-9 mb-3 xs:mt-3 inline-block"
           >
-            <li className="text-secondary-color text-base hover:text-font-color">MDN</li>
+            <li className="text-secondary-color text-base hover:text-font-color">
+              MDN
+            </li>
           </a>
         </ul>
         <ul className="flex flex-col align-middle m-6 xs:mb-1 sm:m-1">
           <span className="p-1 font-semibold text-2xl">Useful Links</span>
-          <a
-            href="https://dev.to/"
-            className="mt-9 mb-3 xs:mt-3 inline-block"
-          >
-            <li className="text-secondary-color text-base hover:text-font-color">dev.to</li>
+          <a href="https://dev.to/" className="mt-9 mb-3 xs:mt-3 inline-block">
+            <li className="text-secondary-color text-base hover:text-font-color">
+              dev.to
+            </li>
           </a>
           <a
             href="https://www.youtube.com/c/WebDevSimplified"
             className="mt-9 mb-3 xs:mt-3 inline-block"
           >
-            <li className="text-secondary-color text-base hover:text-font-color">Kyle</li>
+            <li className="text-secondary-color text-base hover:text-font-color">
+              Kyle
+            </li>
           </a>
           <a
             href="https://www.youtube.com/c/DevEd"
             className="mt-9 mb-3 xs:mt-3 inline-block"
           >
-            <li className="text-secondary-color text-base hover:text-font-color">Ed</li>
+            <li className="text-secondary-color text-base hover:text-font-color">
+              Ed
+            </li>
           </a>
         </ul>
       </div>
