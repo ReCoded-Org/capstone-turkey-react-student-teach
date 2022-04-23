36b242e0d09302fc2c12fbf9601c4a232f408d47
hazar-nas <hazarcan.nas@gmail.com>
2022-04-23 20:47:28 +0300

    eslint

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './redux/store/store';
 import './index.css';
 
