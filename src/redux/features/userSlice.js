8cc3a5fa21f1764f4ce5f44599b2b5eefe61f70b
Ali <iqalisaleh@gmail.com>
2022-04-28 02:56:44 +0300

    fixed darkmode button style bug

--- src/redux/features/userSlice.js
+++ src/redux/features/userSlice.js
@@ -20,7 +20,6 @@ export const login = createAsyncThunk(
   },
 );
 export const passToLocalStorage = (state, action) => {
-  console.log(state, action, 'aaa');
   return (
     !state.user.userInfo.error &&
     localStorage.setItem('userInfo', JSON.stringify(action.payload))
