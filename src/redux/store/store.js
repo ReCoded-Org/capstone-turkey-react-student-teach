caf76b2360504d1bcf01f50f9f94e42c129c695f
hazar-nas <hazarcan.nas@gmail.com>
2022-04-27 17:52:37 +0300

    sigin-with-local-storage

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,11 +2,20 @@ import { configureStore } from '@reduxjs/toolkit';
 import addQuestionSlice from '../features/addQuestionSlice';
 import userSlice from '../features/userSlice';
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : {};
+
+const initialState = {
+  signIn: { user: { userInfo: userInfoFromStorage } },
+};
+
 const store = configureStore({
   reducer: {
     addQuestionReducer: addQuestionSlice,
     signIn: userSlice,
   },
+  preloadedState: initialState,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
