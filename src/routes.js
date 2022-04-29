d46e022443b6f6184d000351ca79799f202de8a9
Enes Koç <suateneskoc@gmail.com>
2022-04-29 20:46:23 +0300

    added single question routing rule

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 export const HOME_ROUTE = '/';
 export const CONTACT_ROUTE = '/contact';
 export const ABOUT_ROUTE = '/about';
-export const QUESTIONS_ROUTE = '/questions';
+export const QUESTIONS_ROUTE = '/question';
 export const USERPROFILE_ROUTE = '/profile';
