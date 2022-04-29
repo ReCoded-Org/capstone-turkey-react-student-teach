db07475a70cb7e4ac49151e36cac34715e7e72e2
hazar-nas <73062961+hazar-nas@users.noreply.github.com>
2022-04-29 03:04:48 +0300

    signup-done (#53)

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -8,8 +8,15 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : {};
 
+const userInfoFromSignUp = localStorage.getItem('signedUser')
+  ? JSON.parse(localStorage.getItem('signedUser'))
+  : {};
+
 const initialState = {
-  signIn: { user: { userInfo: userInfoFromStorage } },
+  signIn: {
+    user: { userInfo: userInfoFromStorage },
+    signUp: { isSignedUp: userInfoFromSignUp },
+  },
 };
 
 const store = configureStore({
