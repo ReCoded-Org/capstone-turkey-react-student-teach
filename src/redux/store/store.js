5f150f1f326ae9c7b40a8b35d131d977a5bb2c89
Enes Koç <suateneskoc@gmail.com>
2022-04-30 18:11:35 +0300

    added slice to store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import addQuestionSlice from '../features/addQuestionSlice';
+import singleQuestionSlice from '../features/singleQuestionSlice';
 
 const store = configureStore({
   reducer: {
     addQuestionReducer: addQuestionSlice,
+    singleQuestionReducer: singleQuestionSlice,
   },
 });
 
