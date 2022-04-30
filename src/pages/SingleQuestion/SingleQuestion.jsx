860bc0f7ffa30f979bcf4088a4273f49b052ca9a
Enes Koç <suateneskoc@gmail.com>
2022-04-30 18:12:14 +0300

    improved single question page

--- src/pages/SingleQuestion/SingleQuestion.jsx
+++ src/pages/SingleQuestion/SingleQuestion.jsx
@@ -1,9 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { getQuestion } from '../../redux/features/singleQuestionSlice';
+import Question from '../../components/Question/Question';
 
-function SingleQuestion() {
+export default function SingleQuestion() {
+  const dispatch = useDispatch();
   const { questionID } = useParams();
-  console.log(questionID);
-  return <div />;
-}
+  useEffect(() => {
+    dispatch(getQuestion(questionID));
+    console.log(questionID);
+  }, [dispatch, questionID]);
 
-export default SingleQuestion;
+  const status = useSelector((state) => state.singleQuestionReducer.status);
+  const question = useSelector((state) => state.singleQuestionReducer.question);
+
+  useEffect(() => {
+    console.log('id', questionID);
+    console.log('quesiton', question);
+  }, [question, questionID]);
+
+  return (
+    <div className="container sm:mx-auto w-10/12 mt-8 pr-2">
+      <div className="flex content-center">
+        <div className="w-full mx-auto w-12/12 sm:w-10/12  xl:w-7/12">
+          <Question status={status} question={question} />
+        </div>
+      </div>
+    </div>
+  );
+}
