9c0a6e36724d71ae5767799ae592a3c156573508
Enes Koç <suateneskoc@gmail.com>
2022-05-01 19:14:14 +0300

    fixed singleQuestionSlice

--- src/pages/SingleQuestion/SingleQuestion.jsx
+++ src/pages/SingleQuestion/SingleQuestion.jsx
@@ -3,17 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getQuestion } from '../../redux/features/singleQuestionSlice';
 import Question from '../../components/Question/Question';
+import Answer from '../../components/Answer/Answer';
 
 export default function SingleQuestion() {
   const dispatch = useDispatch();
   const { questionID } = useParams();
   const status = useSelector((state) => state.singleQuestionReducer.status);
-  const question = useSelector(
-    (state) => state.singleQuestionReducer.question.getQuestionById,
-  );
-  const studentInfo = useSelector(
-    (state) => state.singleQuestionReducer.question.getStudentById,
-  );
+  const question = useSelector((state) => state.singleQuestionReducer.question);
+  const comments = useSelector((state) => state.singleQuestionReducer.comments);
 
   useEffect(() => {
     dispatch(getQuestion(questionID));
@@ -23,17 +20,30 @@ export default function SingleQuestion() {
     <div className="container sm:mx-auto w-10/12 mt-8 pr-2">
       <div className="flex content-center">
         <div className="w-full mx-auto w-12/12 sm:w-10/12  xl:w-7/12">
-          {status === 'loading' ? (
+          {status.question === 'loading' ? (
             <p>loading....</p>
           ) : (
             <div>
-              <Question question={question} studentInfo={studentInfo} />
+              <Question question={question} status={status.question} />
               {question?.comments?.map((comment) => (
                 <div className="flex">
                   <h2 className="mr-2">{comment.creator}</h2>
                   <p>{comment.content}</p>
                 </div>
               ))}
+            </div>
+          )}
+          {status.comments === 'loading' ? (
+            <p>Comments loading</p>
+          ) : (
+            <div>
+              {comments.map((comment) => {
+                return (
+                  <div>
+                    <Answer comment={comment} />
+                  </div>
+                );
+              })}
             </div>
           )}
         </div>
