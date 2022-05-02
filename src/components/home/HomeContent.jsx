e414696a3c78be2183db99a0a1801da7fd72f64d
Ali Saleh <97849626+aliSalehAbdulhadi@users.noreply.github.com>
2022-05-02 13:31:12 +0300

    Add darkmode to localstorage (#63)
    * fixed navbar mobile screen flex bug
    
    * dev
    
    * added darkmode to localstorage

--- src/components/home/HomeContent.jsx
+++ src/components/home/HomeContent.jsx
@@ -15,7 +15,11 @@ function HomeContent() {
   const darkMode = useSelector((state) => state.darkModeReducer.darkMode);
 
   return (
-    <div className={`${darkMode ? 'bg-secondaryDark' : 'bg-zinc-100 '} py-5`}>
+    <div
+      className={`${
+        darkMode ? 'bg-secondaryDark' : 'bg-zinc-100 '
+      } pt-5 pb-[5rem]`}
+    >
       <div
         className={` container mx-auto w-full pt-8 rounded-md transition-all ease-in-out duration-300  text-center lg:text-left ${
           darkMode ? 'bg-primaryDark' : 'bg-white'
