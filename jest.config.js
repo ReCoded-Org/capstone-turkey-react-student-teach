b62e11a4371316cfcd1a71446620316741e82803
Oguz <yilmazoguz@outlook.com>
2022-05-02 20:37:25 +0300

    eslint --fix --ext .js .jsx . in package.json for making it fix the spacing issues etc.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
-module.exports = {
-    transform: {
-      '^.+\\.ts?$': 'ts-jest'
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
-    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-    collectCoverage: true,
-    clearMocks: true,
-    coverageDirectory: "coverage",
-  };
-  
+export const transform = {
+  '^.+\\.ts?$': 'ts-jest',
+  '\\.[jt]sx?$': 'babel-jest',
+};
+export const testRegex = '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$';
+export const moduleFileExtensions = ['ts', 'js', 'json', 'node'];
+export const collectCoverage = true;
+export const clearMocks = true;
+export const coverageDirectory = 'coverage';
