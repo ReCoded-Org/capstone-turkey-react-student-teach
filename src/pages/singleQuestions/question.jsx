599828e167b0f27dba1ad7e9cb1c4892a9242f0f
myusufadiyaman <49249673+MUHAMMED-YUSUF-ADIYAMAN@users.noreply.github.com>
2022-05-03 19:52:32 +0300

    fix eror

--- src/pages/singleQuestions/question.jsx
+++ src/pages/singleQuestions/question.jsx
@@ -10,18 +10,22 @@ import Avatar from '../../assets/images/avatar.jpg';
 
 const question = () => {
   const { id } = useParams();
-  const data = useSelector((state) => state.questions.questions);
+  const data = useSelector((state) => state.questions);
   // eslint-disable-next-line no-underscore-dangle
-  const ques = data?.filter((q) => q?._id === id);
+  const ques = data.questions?.filter((q) => q?._id === id);
 
-  const users = useSelector((state) => state.fetchAllUsers.users);
-  const user = users?.filter((u) => (u.id === ques[0]?.student ? u : false));
+  const users = useSelector((state) => state.fetchAllUsers);
+  const user = users.users?.filter((u) =>
+    u.id === ques[0]?.student ? u : false,
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchQuestions());
     dispatch(fetchAllUsers());
   }, [dispatch]);
+  if (users.status !== 'success' || data.status !== 'success')
+    return <div>looding</div>;
   return (
     <Question
       avatar={user[0]?.avatar === undefined ? Avatar : user[0].avatar}
