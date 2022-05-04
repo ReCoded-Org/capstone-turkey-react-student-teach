5e8cde63cad35097dd7655991143734c70521d6f
Enes Koç <suateneskoc@gmail.com>
2022-05-05 01:19:15 +0300

    add loading animation wip

--- src/pages/SingleQuestion/SingleQuestion.jsx
+++ src/pages/SingleQuestion/SingleQuestion.jsx
@@ -1,52 +1,50 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { getQuestion } from '../../redux/features/singleQuestionSlice';
+import { fetchQuestions } from '../../redux/features/questionsSlice';
+import { fetchAllUsers } from '../../redux/features/fetchAllUsersSlice';
 import Question from '../../components/Question/Question';
-import Answer from '../../components/Answer/Answer';
+// import Answer from '../../components/Answer/Answer';
+import userProfilePlaceholder from '../../assets/images/profilePlaceholer.png';
 
 export default function SingleQuestion() {
   const dispatch = useDispatch();
   const { questionID } = useParams();
+  const data = useSelector((state) => state.questions);
+  // eslint-disable-next-line no-underscore-dangle
+  const ques = data.questions?.filter((q) => q?._id === questionID);
+  const darkMode = useSelector((state) => state.darkModeReducer.darkMode);
   const status = useSelector((state) => state.singleQuestionReducer.status);
-  const question = useSelector((state) => state.singleQuestionReducer.question);
-  const comments = useSelector((state) => state.singleQuestionReducer.comments);
+  //   const question = useSelector((state) => state.singleQuestionReducer.question);
+  //   const comments = useSelector((state) => state.singleQuestionReducer.comments);
+  const users = useSelector((state) => state.fetchAllUsers);
+  const user = users.users?.filter((u) =>
+    u.id === ques[0]?.student ? u : false,
+  );
 
   useEffect(() => {
-    dispatch(getQuestion(questionID));
-  }, [dispatch, questionID]);
+    dispatch(fetchQuestions());
+    dispatch(fetchAllUsers());
+  }, [dispatch]);
+
+  console.log(user);
 
   return (
-    <div className="container sm:mx-auto w-10/12 mt-8 pr-2">
-      <div className="flex content-center">
-        <div className="w-full mx-auto w-12/12 sm:w-10/12  xl:w-7/12">
-          {status.question === 'loading' ? (
-            <p>loading....</p>
-          ) : (
-            <div>
-              <Question question={question} status={status.question} />
-              {question?.comments?.map((comment) => (
-                <div className="flex">
-                  <h2 className="mr-2">{comment.creator}</h2>
-                  <p>{comment.content}</p>
-                </div>
-              ))}
-            </div>
-          )}
-          {status.comments === 'loading' ? (
-            <p>Comments loading</p>
-          ) : (
-            <div>
-              {comments.map((comment) => {
-                return (
-                  <div>
-                    <Answer comment={comment} />
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </div>
+    <div>
+      <div className={`${darkMode ? 'bg-primaryDark' : 'bg-white'} py-10`}>
+        <Question
+          avatar={
+            user[0]?.avatar === undefined
+              ? userProfilePlaceholder
+              : user[0].avatar
+          }
+          userName={`${user[0]?.firstName}  ${user[0]?.lastName}`}
+          questionTitle={ques[0]?.title}
+          questionText={ques[0]?.content}
+          studentId={user[0].id}
+          createdAt={ques[0]?.createdAt}
+          status={status.question}
+        />
       </div>
     </div>
   );
