290e502d9f036beb532eb82f143cbf4dc327913d
myusufadiyaman <49249673+MUHAMMED-YUSUF-ADIYAMAN@users.noreply.github.com>
2022-05-05 13:12:31 +0300

    finish

--- src/components/AllQuestions/AllQuestionsContent.jsx
+++ src/components/AllQuestions/AllQuestionsContent.jsx
@@ -74,7 +74,7 @@ function AllQuestionsContent() {
         </section>
         <div className="mt-5 md:mr-7 lg:mr-10 pb-10">
           {renderQuestion
-            ?.slice(renderQuestion.length - 11, renderQuestion.length)
+            ?.slice(renderQuestion.length - 21, renderQuestion.length)
             .reverse()
             .map((q) => (
               <Question
@@ -91,56 +91,6 @@ function AllQuestionsContent() {
                 }
               />
             ))}
-          <div className="flex flex-col items-center my-12">
-            <div className="flex text-gray-700">
-              <div className="h-12 w-12 mr-1 flex justify-center items-center rounded-full bg-gray-100 cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="100%"
-                  height="100%"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-chevron-left w-6 h-6"
-                >
-                  <polyline points="15 18 9 12 15 6" />
-                </svg>
-              </div>
-              <div className="flex h-12 font-medium rounded-full bg-gray-100">
-                <div className="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full  ">
-                  1
-                </div>
-                <div className="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full bg-[#CA7560] text-white ">
-                  2
-                </div>
-                <div className="w-12 md:flex justify-center items-center hidden  cursor-pointer leading-5 transition duration-150 ease-in  rounded-full  ">
-                  3
-                </div>
-                <div className="w-12 h-12 md:hidden flex justify-center items-center cursor-pointer leading-5 transition duration-150 ease-in rounded-full bg-teal-600 text-white">
-                  2
-                </div>
-              </div>
-              <div className="h-12 w-12 ml-1 flex justify-center items-center rounded-full bg-gray-100 cursor-pointer">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="100%"
-                  height="100%"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-chevron-right w-6 h-6"
-                >
-                  <polyline points="9 18 15 12 9 6" />
-                </svg>
-              </div>
-            </div>
-          </div>
         </div>
         {isSuccess === 'success' ||
         signIn.signUp.status === 'success' ||
